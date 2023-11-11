37a9433a4b67ef2caaf573abfd5c00c8721dc67e
kobe shaw <kobeshaw330@gmail.com>
2023-11-11 09:54:23 -0600

    moved the featured row up abit more

--- my-react-app/src/Featured.js
+++ my-react-app/src/Featured.js
@@ -5,54 +5,68 @@ import DMS from './img/DMS.jpg'
 import OPC from './img/OPC.jpg'
 import MHA from './img/MHA.jpg'
 
+
+
+
 const FeaturedShows = () => {
+
   return (
-    <div className="featured-shows-container">
-      <div className="featured-show">
+    <div className="featured-shows-container"     >
+      <div className="featured-show" >
         {/* Show Image */}
         <img src={JJK} alt="Show 1" />
 
         {/* Show Info */}
         <div className="show-info">
-          <h3>Show Title</h3>
-          <p>Show Description</p>
+          <h3>Jujutsu Kaisen</h3>
+          <p>Yuji Itadori is a boy with tremendous physical strength, though he lives a completely ordinary high school life. <br/>
+            One day, to save a classmate who has been attacked by curses, he eats the finger of Ryomen Sukuna, <br/>
+            taking the curse into his own soul. From then on, he shares one body with Ryomen Sukuna.</p>
         </div>
       </div>
 
-      <div className="featured-show">
+      <div className="featured-show" >
         {/* Show Image */}
-        <img src={DMS} alt="Show 2" />
+        <img src={MHA} alt="Show 1" />
 
         {/* Show Info */}
         <div className="show-info">
-          <h3>Show Title</h3>
-          <p>Show Description</p>
+          <h3>Jujutsu Kaisen</h3>
+          <p>Yuji Itadori is a boy with tremendous physical strength, though he lives a completely ordinary high school life. <br/>
+            One day, to save a classmate who has been attacked by curses, he eats the finger of Ryomen Sukuna, <br/>
+            taking the curse into his own soul. From then on, he shares one body with Ryomen Sukuna.</p>
         </div>
       </div>
 
-      <div className="featured-show">
+      <div className="featured-show" >
         {/* Show Image */}
-        <img src={MHA} alt="Show 3" />
+        <img src={OPC} alt="Show 1" />
 
         {/* Show Info */}
         <div className="show-info">
-          <h3>Show Title</h3>
-          <p>Show Description</p>
+          <h3>Jujutsu Kaisen</h3>
+          <p>Yuji Itadori is a boy with tremendous physical strength, though he lives a completely ordinary high school life. <br/>
+            One day, to save a classmate who has been attacked by curses, he eats the finger of Ryomen Sukuna, <br/>
+            taking the curse into his own soul. From then on, he shares one body with Ryomen Sukuna.</p>
         </div>
       </div>
 
-      <div className="featured-show">
+      <div className="featured-show" >
         {/* Show Image */}
-        <img src={OPC} alt="Show 4" />
+        <img src={DMS} alt="Show 1" />
 
         {/* Show Info */}
         <div className="show-info">
-          <h3>Show Title</h3>
-          <p>Show Description</p>
+          <h3>Jujutsu Kaisen</h3>
+          <p>Yuji Itadori is a boy with tremendous physical strength, though he lives a completely ordinary high school life. <br/>
+            One day, to save a classmate who has been attacked by curses, he eats the finger of Ryomen Sukuna, <br/>
+            taking the curse into his own soul. From then on, he shares one body with Ryomen Sukuna.</p>
         </div>
       </div>
 
     </div>
+
+
   );
 };
 
